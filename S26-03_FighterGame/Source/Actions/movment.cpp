815784c90fa99831424fb125af8a65e46cfb97dd
Muaaz Tariq <muaaztariq109@gmail.com>
2026-04-19 18:12:40 +0500

    collision added. Goku sprite not loading movement animation with A key

--- S26-03_FighterGame/Source/Actions/movment.cpp
+++ S26-03_FighterGame/Source/Actions/movment.cpp
@@ -1,27 +1,63 @@
 #include "sfml.h"
 
 void movement::Movement()
-{	
-	player->IsMoving = false;
+{
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)&& player->PlayerNumber == 1)
+	player->IsMoving = false;
+	bool p1movingRight = false;
+	bool p1movingLeft = false;
+	bool p2movingLeft = false;
+	bool p2movingRight = false;
+
+
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && player->PlayerNumber == 1)
 	{
-		player->Sprite.move(VelocityX * dt, 0.f);
-		player->IsMoving = true;
+		p1movingRight = true;
+
+
 	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)&& player->PlayerNumber == 1)
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && player->PlayerNumber == 1)
 	{
-		player->Sprite.move(-VelocityX * dt, 0.f);
-		player->IsMoving = true;
+		p1movingLeft = true;
 	}
-	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)&& player->PlayerNumber == 2)
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && player->PlayerNumber == 2)
 	{
-		player->Sprite.move(VelocityX * dt, 0.f);
-		player->IsMoving = true;
+		p2movingRight = true;
 	}
-	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)&& player->PlayerNumber == 2)
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && player->PlayerNumber == 2)
 	{
-		player->Sprite.move(-VelocityX * dt, 0.f);
-		player->IsMoving = true;
+		p2movingLeft = true;
+
 	}
+
+	sf::FloatRect p1bound = player1.Sprite.getGlobalBounds();
+	sf::FloatRect p2bound = player2.Sprite.getGlobalBounds();
+
+	if (p1movingRight) {
+		if (!p1bound.intersects(p2bound) || player1.Sprite.getPosition().x > player2.Sprite.getPosition().x) {
+
+			player->Sprite.move(VelocityX * dt, 0.f);
+			player->IsMoving = true;
+		}
+
+	}
+	if (p1movingLeft) {
+		if (!p1bound.intersects(p2bound) || player1.Sprite.getPosition().x < player2.Sprite.getPosition().x) {
+			player->Sprite.move(-VelocityX * dt, 0.f);
+		}
+	}
+	if (p2movingRight) {
+		if (!p2bound.intersects(p1bound) || player2.Sprite.getPosition().x > player1.Sprite.getPosition().x) {
+			player->Sprite.move(VelocityX * dt, 0.f);
+			player->IsMoving = true;
+		}
+	}
+	if (p2movingLeft) {
+		if (!p2bound.intersects(p1bound) || player2.Sprite.getPosition().x < player1.Sprite.getPosition().x) {
+			player->Sprite.move(-VelocityX * dt, 0.f);
+			player->IsMoving = true;
+		}
+	}
+
 }
