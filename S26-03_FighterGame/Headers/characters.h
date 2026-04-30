88bb6f240cd8d62c0c1b1b6f4952e96e75a15cfc
AbubakarSaleem01 <l250018@lhr.nu.edu.pk>
2026-05-01 02:15:34 +0500

    Health Bar started

--- S26-03_FighterGame/Headers/characters.h
+++ S26-03_FighterGame/Headers/characters.h
@@ -2,9 +2,30 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-class cell
+class HealthBar {
+private:
+    float maxHealth;
+    float currentHealth;
+
+    sf::RectangleShape backgroundBar;
+    sf::RectangleShape foregroundBar;
+
+public:
+    
+    HealthBar(float maxHealthValue, sf::Vector2f position);
+    void TakeDamage(float damage);
+    void Heal(float amount);
+    void Draw(sf::RenderWindow& window);
+    void SetPosition(sf::Vector2f pos);
+
+    float GetCurrentHealth() const { return currentHealth; }
+    bool IsDead() const { return currentHealth <= 0; }
+};
+
+class cell : public HealthBar
 {
 public:
+    HealthBar healthBar;
     virtual void LoadTexture
         (std::vector<sf::Texture>&,
         std::vector<sf::Texture>&,
@@ -15,6 +36,7 @@ public:
 class goku
 {
 public:
+    HealthBar healthBar;
     virtual void LoadTexture
        (std::vector<sf::Texture>&,
         std::vector<sf::Texture>&,
@@ -25,6 +47,7 @@ public:
 class freeza
 {
 public:
+    HealthBar healthBar;
     virtual void LoadTexture
        (std::vector<sf::Texture>&,
         std::vector<sf::Texture>&,
@@ -50,4 +73,5 @@ public:
     {
         SetCharacterTexture();
     }
+    
 };
