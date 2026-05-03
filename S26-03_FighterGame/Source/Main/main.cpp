091583d44f6cd2983d1223d36b9f012a0b8964dd
AbubakarSaleem01 <l250018@lhr.nu.edu.pk>
2026-05-03 22:31:57 +0500

    Added Background Sound for the whole game window

--- S26-03_FighterGame/Source/Main/main.cpp
+++ S26-03_FighterGame/Source/Main/main.cpp
@@ -2,14 +2,21 @@
 Players player1;
 Players player2;
 Background gameBackground;
+SoundManager soundManager;
 
 int main()
 {
 	window GameWindow;
 	showLoadingScreen(GameWindow.MainWindow);
+
+	//background sound add-ons
 	gameBackground.loadBackground(GameWindow.MainWindow);
+	if (!soundManager.loadMusic())
+		return -1;
+	soundManager.playBackground();
+
 	while (GameWindow.WindowisOpen())
-	{	
+	{
 		DeltaTime();
 		while (GameWindow.MainWindow.pollEvent(GameWindow.event))
 		{
