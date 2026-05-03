7cf7209298a964c3a5ec71a28da97e845643bb44
capitanabcd <abullahsajid@gmail.com>
2026-05-03 07:55:15 +0500

    non hard code

--- S26-03_FighterGame/Source/Animations/BeamAnimation.cpp
+++ S26-03_FighterGame/Source/Animations/BeamAnimation.cpp
@@ -1,26 +1,30 @@
 #include "sfml.h"
 
 void animations::BeamAnimation() {
+    if (player->clock.getElapsedTime().asSeconds() >= 0.1f)
+    {
+        if (player->isbeaming)
+        {
+            if (player->BeamFrames < player->BeamTextures.size() - 1) {
+                player->Sprite.setTexture(player->BeamTextures[player->BeamFrames], true);
+                player->clock.restart();
+                player->BeamFrames++;
+            }
+            else if (player->BeamFrames == player->BeamTextures.size() - 1) {
+                if (!player->fireRanged) {
+                    player->fireRanged = true;
+                }
+                player->Sprite.setTexture(player->BeamTextures[player->BeamFrames], true);
+            }
 
-	if (player->clock.getElapsedTime().asSeconds() >= 0.1f)
-	{
-		if (player->isbeaming)
-		{
-			if (player->BeamFrames == 9 && !player->fireRanged) {
-				player->fireRanged = true;
-			}
-			player->Sprite.setTexture(player->BeamTextures[player->BeamFrames], true);
-			player->clock.restart();
-			player->BeamFrames++;
-
-			
-
-			if (player->BeamFrames >= player->BeamTextures.size()) {
-				player->Sprite.setTexture(player->IdleTextures[0], true);
-				player->BeamFrames = 0;
-				player->isbeaming = false;
-			}
-			RevertOrigin();
-		}
-	}
+            RevertOrigin();
+        }
+    }
+}
+void animations::EndBeamAnimation() {
+    if (player->isbeaming) {
+        player->isbeaming = false;
+        player->BeamFrames = 0;
+        player->Sprite.setTexture(player->IdleTextures[0], true);
+    }
 }
--- S26-03_FighterGame/Source/Animations/RangedAttackAnimation.cpp
+++ S26-03_FighterGame/Source/Animations/RangedAttackAnimation.cpp
@@ -1,25 +1,37 @@
 #include "sfml.h"
 
 void animations::RangedAttackAnimation() {
+    if (player->clock.getElapsedTime().asSeconds() >= 0.009f)
+    {
+        if (player->fireRanged)
+        {
+            sf::FloatRect characterBounds = player->Sprite.getGlobalBounds();
 
-	if (player->clock.getElapsedTime().asSeconds() >= 0.02f)
-	{
-		if (player->fireRanged)
+            float spawnX = player->Sprite.getPosition().x;
+            float spawnY = player->Sprite.getPosition().y/2;
 
-		{
-			player->RangedAttack.setPosition(player->Sprite.getPosition().x + 50, player->Sprite.getPosition().y - 300);
-			invert();
-			player->RangedAttack.setTexture(player->RangedTexture[player->RangedFrames], true);
-			player->clock.restart();
-			player->RangedFrames++;
+            if (player->MovingRight) {
+                spawnX += characterBounds.width/2;
+            }
+            else {
+                spawnX -= characterBounds.width/2;
+            }
+            player->RangedAttack.setPosition(spawnX, spawnY);
 
-			if (player->RangedFrames >= player->RangedTexture.size()) {
-				player->RangedAttack.setTexture(player->EmptyTexture, true);
-				player->RangedFrames = 0;
-				player->fireRanged = false;
-			}
-			invert();
-			RevertOrigin();
-		}
-	}
+            invert();
+            player->RangedAttack.setTexture(player->RangedTexture[player->RangedFrames], true);
+            player->clock.restart();
+            player->RangedFrames++;
+
+            if (player->RangedFrames >= player->RangedTexture.size()) {
+                player->RangedAttack.setTexture(player->EmptyTexture, true);
+                player->RangedFrames = 0;
+                player->fireRanged = false;
+                EndBeamAnimation();
+            }
+
+            invert();
+            RevertOrigin();
+        }
+    }
 }
